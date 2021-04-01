3aecb63d65f17a4add92a56929e08c5de28d84e3
Mork Chase <817251@stma.k12.mn.us>
2021-04-01 09:52:10 -0500

    comparisonTry1

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if int(x > y):
+        return  True
+    else:
+        return False
+
+def lessThan(x, y):
+    if int(x < y):
+        return True
+    else:
+        return False
+
+def equalTo(x, y):
+    if int(x == y):
+        return True
+    else:
+        return False
+
+def greaterOrEqual(x, y):
+    if int(x >= y):
+        return True
+    else:
+        return False
+
+def lessorEqual(x, y):
+    if int(x <= y):
+        return True
+    else:
+        return False
+
+print(greaterOrEqual(156, 155))
